b844ddb4c5a5088b660e8d03aa190dc68bf11623
AdamLagerqvist <adam.lagerqvist@elev.ga.ntig.se>
2021-10-01 11:24:25 +0200

    🗃️ databas api

---
+++ src/_data/dog.js
@@ -0,0 +1,16 @@
+const Cache = require('@11ty/eleventy-cache-assets');
+
+const index = Math.random()*100;
+
+module.exports = async function () {
+    try {
+        let json = await Cache(`https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=${index}`, {
+            duration: '1d', // 1 day
+            type: 'json' // also supports "text" or "buffer"
+        });
+        return json;
+    } catch (e) {
+        console.log(e);
+        return {};
+    }
+};
